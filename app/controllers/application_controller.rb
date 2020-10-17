3715f81a0ab55bcd1438083378ba2e50194409f1
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-10-17 17:49:41 +0900

    devise 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   def user
     if user_signed_in?
       @user = current_user
